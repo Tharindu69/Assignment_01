0857595147d81538f339327695026c3715467620
Tharindu69 <2019cs169@stu.ucsc.lk>
2021-05-16 08:15:31 +0530

    adding scala files

---
+++ Q02.scala
@@ -0,0 +1,6 @@
+object Q02 extends App{
+def vol(r:Double)=1.33*3.14*r*r*r;
+
+println(vol(5));
+
+}
---
+++ Q03.scala
@@ -0,0 +1,8 @@
+object Q03 extends App{
+
+  def WhCost(x:Double)=24.95*x*0.4+150+7.5;
+
+  println(WhCost(60));
+
+
+}
